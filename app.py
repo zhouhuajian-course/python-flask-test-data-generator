b2f07f1eab020492835c7f887ba335c266af088f
zhouhuajian <zhouhuajian@email.com>
2022-07-27 12:40:54 +0800

    前端页面开发

---
+++ app.py
@@ -0,0 +1,17 @@
+"""
+测试数据生成器
+
+@author  : zhouhuajian
+@version : v1.0
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
